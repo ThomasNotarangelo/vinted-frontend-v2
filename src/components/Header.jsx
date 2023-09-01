9e39a45dc84d504f616e5bc8f10427164bbc0ba0
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-09-01 11:59:40 +0200

    added hero and css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <header className="header-container">
+    <div className="header-container">
       <Link to="/">
         <img className="vinted-logo" src={logo} alt="Logo Vinted" />
       </Link>
@@ -19,9 +19,20 @@ const Header = () => {
           />
         </div>
       </div>
-      <button>Connexion</button>
-      <button>S'inscrire</button>
-    </header>
+      <div>
+        <button className="header-button button-login-signup button-signup">
+          S'inscrire
+        </button>
+        <button className="header-button button-login-signup">
+          Se connecter
+        </button>
+      </div>
+      <div>
+        <button className="header-button button-sold">
+          Vends tes articles
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import tear from "../assets/tear.svg";
 
 const Home = () => {
   const [data, setData] = useState({});
@@ -24,40 +25,54 @@ const Home = () => {
   return isLoading ? (
     <p>En cours de chargement ... </p>
   ) : (
-    <div>
-      {data.offers.map((offer, index) => {
-        return (
-          <Link key={offer._id} to={`/offer/${offer._id}`}>
-            <article>
-              <div>
-                {offer.owner.account.avatar && (
+    <>
+      <div className="home-hero">
+        <img src={tear} alt="forme" className="home-hero-forme" />
+      </div>
+      <div className="container">
+        <div className="offerList">
+          {data.offers.map((offer, index) => {
+            return (
+              <Link key={offer._id} to={`/offer/${offer._id}`}>
+                <article className="article">
+                  <div className="user">
+                    {offer.owner.account.avatar && (
+                      <img
+                        className="avatar"
+                        src={offer.owner.account.avatar.secure_url}
+                        alt="user avatar"
+                      />
+                    )}
+                    <span className="username">
+                      {offer.owner.account.username}
+                    </span>
+                  </div>
+                  <p>{offer.product_name}</p>
                   <img
-                    src={offer.owner.account.avatar.secure_url}
-                    alt="user avatar"
+                    className="productImg"
+                    src={offer.product_image.secure_url}
+                    alt="clothe"
                   />
-                )}{" "}
-                <span>{offer.owner.account.username}</span>
-              </div>
-              <p>{offer.product_name}</p>
-              <img src={offer.product_image.secure_url} alt="clothe" />
-              <p>{offer.product_price} €</p>
-              <div>
-                {offer.product_details.reverse().map((detail, index) => {
-                  //   console.log(detail);
-                  if (detail.MARQUE) {
-                    return <p key={index}>{detail.MARQUE}</p>;
-                  } else if (detail.TAILLE) {
-                    return <p key={index}> {detail.TAILLE} </p>;
-                  } else {
-                    return null;
-                  }
-                })}
-              </div>
-            </article>
-          </Link>
-        );
-      })}
-    </div>
+                  <p>{offer.product_price} €</p>
+                  <div>
+                    {offer.product_details.reverse().map((detail, index) => {
+                      //   console.log(detail);
+                      if (detail.MARQUE) {
+                        return <p key={index}>{detail.MARQUE}</p>;
+                      } else if (detail.TAILLE) {
+                        return <p key={index}> {detail.TAILLE} </p>;
+                      } else {
+                        return null;
+                      }
+                    })}
+                  </div>
+                </article>
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
